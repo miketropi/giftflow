7a69601edb60497dc92fd392f0c64954f96138f4
Mike <mike.beplus@gmail.com>
2026-01-12 16:10:49 +0700

    Update

--- includes/gateways/class-paypal.php
+++ includes/gateways/class-paypal.php
@@ -298,6 +298,7 @@ class PayPal_Gateway extends Gateway_Base {
 
 		ob_start();
 		$icons = array(
+			'error' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-alert-icon lucide-circle-alert"><circle cx="12" cy="12" r="10"/><line x1="12" x2="12" y1="8" y2="12"/><line x1="12" x2="12.01" y1="16" y2="16"/></svg>',
 			'checked' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-check-icon lucide-badge-check"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="m9 12 2 2 4-4"/></svg>',
 		);
 
@@ -328,7 +329,16 @@ class PayPal_Gateway extends Gateway_Base {
 			</div>
 
 			<?php // PayPal buttons container. ?>
-			<div class="donation-form__field">
+			<div 
+		class="donation-form__field"
+		data-custom-validate="true" 
+		data-custom-validate-status="false" >
+		<div class="donation-form__field-error custom-error-message" style="margin-bottom: 10px;">
+			<?php echo wp_kses( $icons['error'], giftflow_allowed_svg_tags() ); ?>
+			<span class="custom-error-message-text">
+			<?php esc_html_e( 'Please complete the payment process with PayPal.', 'giftflow' ); ?>
+			</span>
+		</div>
 				<div id="giftflow-paypal-button-container"></div>
 			</div>
 		</div>
